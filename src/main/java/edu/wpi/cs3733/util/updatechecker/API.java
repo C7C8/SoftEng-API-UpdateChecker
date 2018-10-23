7f4a5cc3301c7bb2e66b4a3aecf4f7d0f812f329
Sourec <tothemyers@gmail.com>
2018-10-23 14:26:57 -0400

    Add definition for API object

--- src/main/java/edu/wpi/cs3733/util/updatechecker/API.java
+++ src/main/java/edu/wpi/cs3733/util/updatechecker/API.java
@@ -1,4 +1,18 @@
 package edu.wpi.cs3733.util.updatechecker;
 
 public class API {
+	public String id = "";
+	public String name = "";
+	public String version = "";
+	public int size = 0;
+	public String contact = "";
+	public String gradle = "";
+	public String description = "";
+	public String image = "";
+	public Long updated = 0l;
+	public String term = "";
+	public Integer year = 0;
+	public String team = "";
+	public String creator = "";
+	public String[] history = {};
 }
